70f546490fc43b491aad0be8bdc52575b3f01978
itsmehemm <hemanthprasathmurali@gmail.com>
2018-11-27 16:04:52 +0530

    DB test for registerUser, getUserInfo and getSchemas

---
+++ src/db/test.js
@@ -0,0 +1,33 @@
+const db = require('./index');
+
+// db.getSchemas().then(schemaList => console.log(schemaList));
+
+// db.registerUser({
+// 	type: 'email',
+// 	username: 'hemm',
+// 	firstname: 'Hemanthtest',
+// 	lastname: 'email test',
+// 	signuptype: '1',
+// 	signuptypelabel: 'email',
+// 	email: 'hemm@paypal.com',
+// 	password: 'buddy123'
+// })
+// .then((response) => console.log(response))
+// .catch(error => console.log(error));
+
+// db.registerUser({
+// 	type: 'phonenumber',
+// 	username: 'hemm2',
+// 	firstname: 'Hemanthtest',
+// 	lastname: 'email test',
+// 	signuptype: '2',
+// 	signuptypelabel: 'phonenumber',
+// 	password: 'buddy123',
+// 	phonenumber: '9898989898'
+// })
+// .then((response) => console.log(response))
+// .catch(error => console.log(error));
+
+// db.getUserInfo({
+// 	email: 'hemm@paypal.com'
+// }).then((userInfo) => console.log(userInfo))
